cffa5a0205f73881ba53ea1ccaa7d9cd170a56ba
Mohammad <s12010803@stu.najah.edu>
2024-07-09 13:05:43 +0300

    Add project skelton and some containerization setups

---
+++ TAB/TAB.Infrastructure/DependencyInjection.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace TAB.Infrastructure;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddInfrastructure(this IServiceCollection services) =>
+        services;
+}
---
+++ TAB/TAB.Persistence/DependencyInjection.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace TAB.Persistence;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddPersistence(this IServiceCollection services) => services;
+}
