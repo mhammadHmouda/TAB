a92158a7574f90ebf13b92c9c5fec6a0a8bf0d08
Mohammad <s12010803@stu.najah.edu>
2024-07-29 00:12:30 +0300

    Add booking repository

---
+++ src/TAB.Domain/Features/BookingManagement/Repositories/IBookingRepository.cs
@@ -0,0 +1,6 @@
+﻿using TAB.Domain.Core.Interfaces;
+using TAB.Domain.Features.BookingManagement.Entities;
+
+namespace TAB.Domain.Features.BookingManagement.Repositories;
+
+public interface IBookingRepository : IRepository<Booking> { }
---
+++ src/TAB.Persistence/Repositories/BookingManagement/BookingRepository.cs
@@ -0,0 +1,12 @@
+﻿using TAB.Application.Core.Interfaces.Data;
+using TAB.Domain.Features.BookingManagement.Entities;
+using TAB.Domain.Features.BookingManagement.Repositories;
+using TAB.Persistence.Repositories.Abstractions;
+
+namespace TAB.Persistence.Repositories.BookingManagement;
+
+public class BookingRepository : BaseRepository<Booking>, IBookingRepository
+{
+    public BookingRepository(IDbContext dbContext)
+        : base(dbContext) { }
+}
