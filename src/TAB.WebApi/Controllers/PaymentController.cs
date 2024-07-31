9af055fa1d90dc265d7e90763b47fc62d85d5270
Mohammad <s12010803@stu.najah.edu>
2024-08-01 01:04:26 +0300

    Add endpoint for checkout and success booking

---
+++ src/TAB.WebApi/Controllers/PaymentController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TAB.Application.Features.BookingManagement.SuccessPayment;
+using TAB.Domain.Core.Shared.Result;
+using TAB.WebApi.Abstractions;
+using TAB.WebApi.Attributes;
+using TAB.WebApi.Contracts;
+
+namespace TAB.WebApi.Controllers;
+
+/// <summary>
+/// Payment controller.
+/// </summary>
+[TokenValidation]
+public class PaymentController : ApiController
+{
+    /// <summary>
+    /// Payment success endpoint.
+    /// </summary>
+    /// <param name="sessionId">Session ID.</param>
+    /// <returns>Success payment response.</returns>
+    /// <response code="200">Success payment response.</response>
+    /// <response code="400">Error response.</response>
+    [HttpGet(ApiRoutes.Payment.Success)]
+    public async Task<IActionResult> Success(string sessionId) =>
+        await Result
+            .Create(sessionId)
+            .Map(x => new SuccessPaymentCommand(x))
+            .Bind(x => Mediator.Send(x))
+            .Match(() => Ok("The booking payed successfully!"), BadRequest);
+}
