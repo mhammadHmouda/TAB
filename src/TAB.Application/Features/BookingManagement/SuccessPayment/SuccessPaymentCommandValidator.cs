fdb32f84d588366e3d3032334279d7427d443043
Mohammad <s12010803@stu.najah.edu>
2024-08-01 00:49:47 +0300

    Add use case for success payment

---
+++ src/TAB.Application/Features/BookingManagement/SuccessPayment/SuccessPaymentCommandValidator.cs
@@ -0,0 +1,11 @@
+﻿using FluentValidation;
+
+namespace TAB.Application.Features.BookingManagement.SuccessPayment;
+
+public class SuccessPaymentCommandValidator : AbstractValidator<SuccessPaymentCommand>
+{
+    public SuccessPaymentCommandValidator()
+    {
+        RuleFor(x => x.SessionId).NotEmpty().WithMessage("SessionId is required.");
+    }
+}
