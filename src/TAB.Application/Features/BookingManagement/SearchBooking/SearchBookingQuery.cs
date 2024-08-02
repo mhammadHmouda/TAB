8edabf68ca3358e745f099f679b6c2a07a53cbc7
Mohammad <s12010803@stu.najah.edu>
2024-08-03 01:30:49 +0300

    Add use case for search booking query

---
+++ src/TAB.Application/Features/BookingManagement/SearchBooking/SearchBookingQuery.cs
@@ -0,0 +1,8 @@
+﻿using TAB.Application.Core.Contracts;
+using TAB.Contracts.Features.BookingManagement;
+using TAB.Domain.Core.Shared.Result;
+
+namespace TAB.Application.Features.BookingManagement.SearchBooking;
+
+public record SearchBookingQuery(int Page, int PageSize, string? Filters, string? Sorting)
+    : IQuery<Result<PagedList<BookingResponse>>>;
