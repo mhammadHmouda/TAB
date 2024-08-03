d5bd59fad9a52f6ba5348aabe2c6423c76e3b283
Mohammad <s12010803@stu.najah.edu>
2024-08-04 01:08:06 +0300

    Add use case for get recent visitis hotels

---
+++ src/TAB.Contracts/Features/HotelManagement/Hotels/RecentVisitResponse.cs
@@ -0,0 +1,13 @@
+﻿using TAB.Domain.Features.HotelManagement.ValueObjects;
+
+namespace TAB.Contracts.Features.HotelManagement.Hotels;
+
+public record RecentVisitResponse(
+    string HotelName,
+    string City,
+    string ThumbnailUrl,
+    int Rating,
+    Money Price,
+    DateTime CheckInDate,
+    DateTime CheckOutDate
+);
